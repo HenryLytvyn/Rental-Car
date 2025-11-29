9c010c5b24bd7ce2d5c0575976c8d86d28fc2fd7
Henry <hennadii.lytvyn@gmail.com>
2025-11-29 22:00:12 +0000

    openGraph picture

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'ac.goit.global' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'ac.goit.global' },
+      { protocol: 'https', hostname: '*.vercel.app' },
+    ],
   },
 };
 
