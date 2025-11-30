ad8bbe91a783297583b41516f45ca01ef9ed4bae
Henry <hennadii.lytvyn@gmail.com>
2025-11-30 15:06:46 +0000

    catalog bag fix

--- utils/mileAgeValidation.ts
+++ utils/mileAgeValidation.ts
@@ -1,29 +1,3 @@
-// 'use client';
-
-// import useCarsFilters from '@/lib/store/CarsFiltersStore';
-// import { QueryCarsType } from '@/types/apiRequest/apiRequest';
-
-// export default async function mileAgeValidation() {
-//   const { queryStore, setQueryStore } = useCarsFilters();
-
-//   if (queryStore.minMileage !== '' && queryStore.maxMileage !== '') {
-//     const minMileageNumber = Number(queryStore.minMileage);
-//     const maxMileageNumber = Number(queryStore.maxMileage);
-
-//     if (maxMileageNumber < minMileageNumber) {
-//       setQueryStore({
-//         ...queryStore,
-//         maxMileage: String(minMileageNumber + 500),
-//       });
-
-//       await new Promise(r => setTimeout(r));
-//       // waiting one tick of the event loop so React can process the state update and trigger a re-render
-//     }
-//   }
-
-//   return;
-// }
-
 import useCarsFilters from '@/lib/store/CarsFiltersStore';
 
 export default async function mileAgeValidation() {
